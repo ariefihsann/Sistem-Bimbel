36bd00986885853f628442830f03859a2aa736fc
ariefihsann <arifmuhammadihsan27@gmail.com>
2025-11-10 16:52:57 +0700

    Dashboard Admin

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="app-content-header">
+    <div class="container-fluid">
+        <h3 class="mb-0">Dashboard</h3>
+    </div>
+</div>
+
+<div class="app-content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <h3>Tes Berhasil!</h3>
+                <p>Layout master dan view anak sudah terhubung.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin', function () {
+    // Ini akan memuat file: resources/views/dashboard.blade.php
+    return view('dashboard');
 });
