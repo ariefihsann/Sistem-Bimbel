e95513832e8d014c53ee90ae8387dcb1b4b19ad3
aishaazky <email-github-syaflorence13@gmail.com>
2025-11-26 16:20:23 +0700

    perbaikan sidebar siswa

--- resources/views/partials/sidebar-student.blade.php
+++ resources/views/partials/sidebar-student.blade.php
@@ -11,42 +11,10 @@
         </li>
 
         {{-- Modul Belajar --}}
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('modul*') ? 'active' : '' }}"
-                href="{{ url('/modul/1/materi') }}">
-                <i class="fas fa-book-open"></i>
-                <span>Modul Belajar</span>
-            </a>
-        </li>
 
         {{-- Progress (optional) --}}
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('progress*') ? 'active' : '' }}"
-                href="#">
-                <i class="fas fa-chart-line"></i>
-                <span>Progress</span>
-            </a>
-        </li>
 
-        {{-- Admin ONLY: Users --}}
-        @if(Auth::user()->role_id == 1)
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('admin/users*') ? 'active' : '' }}"
-                href="{{ route('admin.users.index') }}">
-                <i class="fas fa-users"></i>
-                <span>Users</span>
-            </a>
-        </li>
-        @endif
 
-        {{-- Profil --}}
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('profile') ? 'active' : '' }}"
-                href="{{ route('profile.edit') }}">
-                <i class="fas fa-user"></i>
-                <span>Profil Saya</span>
-            </a>
-        </li>
 
     </ul>
 </div>
